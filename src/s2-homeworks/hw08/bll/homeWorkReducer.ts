cd0a5846195906170fe68dc8284b84ae212eff2a
AnnySad <anna.sadovskaya555@gmail.com>
2023-02-02 18:28:16 +0300

    8hw-2-сортировка по имени, фильтрация

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,52 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
+        case "sort": {
+           /* return [...state]
+                .sort((a, b) => {
+                    return a.name > b.name
+                        ? (action.payload === 'up'
+                            ? 1
+                            : -1 )
+                        : a.name < b.name
+                            ? (action.payload === 'up'
+                                ? -1
+                                : 1 )
+                            : 0
+                })*/
+            // by name
+            if (action.payload === "up") {
+            } else {
+                return [...state]
+                    .sort(function (a, b) {
+                    return a.name > b.name ? -1 : a.name < b.name ? 1 : 0
+                })
 
-            return state // need to fix
+            }
+
+            return action.payload === "up"
+                ? [...state].sort((a, b) => {
+
+                    return a.name > b.name ? 1 : a.name < b.name ? -1 : 0
+                })
+
+                : [...state].sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return -1
+                    }
+                    if (a.name < b.name) {
+                        return 1
+                    }
+                    return 0
+                })
         }
+
+
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
