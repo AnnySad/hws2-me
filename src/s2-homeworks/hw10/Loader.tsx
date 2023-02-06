89832943430d28785f424844be081313d5c23cc2
AnnySad <anna.sadovskaya555@gmail.com>
2023-02-06 19:37:45 +0300

    10hw-2-loading...picture

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,6 +1,8 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}
 
+/*export const Loader = () => <div className={s.loader}/>*/
+export const Loader = () => <div className={s.loader}>
+    <img src={ "https://i.gifer.com/origin/9c/9c2ffe7d2a436078cf4786828d829b39_w200.webp"}/>
+    </div>
 
-/>
